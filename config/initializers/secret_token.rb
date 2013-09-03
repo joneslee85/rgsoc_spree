0256687ca7c176c0b03f8ba7f2fd553b6b1b2efe
Trung Lê <trung.le@ruby-journal.com>
2013-09-03 20:42:52 +1000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RgsocSpree::Application.config.secret_token = '574f1121a1c8f67f241c4d8653d82c1b28177b1a38579769229d4dff2e8fef76f91813f2dd65d7969023bdf149cad4f35bb81ffe2415a73a2d0042a6402d88cd'
