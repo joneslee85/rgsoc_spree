93ed674d082f47d4e485f31fe990e7638185c21c
Trung Lê <trung.le@ruby-journal.com>
2013-09-03 23:25:11 +1000

    Add love test into footer-right

---
+++ app/overrides/change_footer.rb
@@ -0,0 +1,4 @@
+Deface::Override.new(virtual_path:  'spree/shared/_footer',
+                     insert_top:    'div#footer-right',
+                     text:          '<p>Made with love by Spree Girl</p>',
+                     name:          'spree_girl_footer')
